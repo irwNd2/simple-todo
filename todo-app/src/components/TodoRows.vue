8826aa464de71d0d259ffbf0bcefeb0c448d3c5c
irwNd2 <irwandduadua@gmail.com>
2022-12-27 00:08:02 +0800

    CRD Todo

---
+++ todo-app/src/components/TodoRows.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+    props: ['todo', 'deleteTodo'],
+
+}
+</script>
+
+<template>
+    <tr>
+        <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+            {{ todo.id }}
+        </td>
+        <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+            {{ todo.title }}
+        </td>
+        <td class="whitespace-nowrap px-4 py-2 text-gray-700"> {{ todo.description }} </td>
+        <td class="whitespace-nowrap px-4 py-2 text-gray-700"> {{ todo.completed }} </td>
+        <td class="whitespace-nowrap px-4 py-2 text-gray-700 text-center"><button
+                class="inline-block px-4 py-1.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >Detail</button> <button
+                class="inline-block px-4 py-1.5 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out"
+                >Update</button> <button
+                class="inline-block px-4 py-1.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
+                @click.prevent="deleteTodo(todo.id)" >Delete</button></td>
+    </tr>
+</template>
---
+++ todo-app/src/stores/todo.js
@@ -0,0 +1,68 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import router from "../router";
+
+export const useTodoStore = defineStore("todo", {
+  state: () => ({
+    todos: [],
+    todo: {},
+  }),
+  actions: {
+    async fetchTodos() {
+      try {
+        const { data } = await axios.get("http://localhost:3000/todos");
+        this.todos = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    async deleteTodoById(id) {
+      try {
+        await axios.delete(`http://localhost:3000/todos/${id}`);
+        this.todos = this.todos.filter((todo) => todo.id !== id);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    async addTodoHandler(todo) {
+      try {
+        const { data } = await axios.post("http://localhost:3000/todos", todo);
+        this.todos.push(data);
+        router.push("/")
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    async fetchTodoById(id) {
+        try {
+            const { data } = await axios.get(`http://localhost:3000/todos/${id}`);
+            this.todo = data;
+        } catch (error) {
+            console.log(error);
+        }
+        },
+
+    async updateTodoById(id, todo) {
+        try {
+            await axios.put(`http://localhost:3000/todos/${id}`, todo);
+            this.todos = this.todos.map((todo) => todo.id === id ? { ...todo, ...todo } : todo);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    async completeTodoHandler(id) {
+        try {
+            const todo = await axios.get(`http://localhost:3000/todos/${id}`);
+            const updateTodo = { ...todo.data, completed: !todo.data.completed };
+            const { data } = await axios.put(`http://localhost:3000/todos/${id}`, updateTodo);
+            this.todos = this.todos.map((todo) => todo.id === id ? { ...todo, completed: data.completed } : todo);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+  },
+});
